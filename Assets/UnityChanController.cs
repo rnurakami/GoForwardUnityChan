3950c277bd54af6a0ab3faaebbce51b95d699ae8
rnurakami <mariamanzini8@gmail.com>
2019-07-16 21:10:59 +0900

    Make unity-chan running

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+
+    Animator animator;
+
+    private float groundLevel = -3.0f;
+
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        this.animator.SetFloat("Horizontal", 1);
+
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+
+    }
+}
